3f123d0aa83faa8d678a8dba8c3d79ab0e157f98
Naeem Mannan <nmannan97@gmail.com>
2022-09-08 21:50:08 -0700

    general changes

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
     <div className="App">
 
       <Routes>
-        <Route exact path='/' element={<About/>} />
-        <Route exact path='/projects' element={<Projects/>}/>
-        <Route exact path='/interests' element={<Interests/>}/>
+        <Route exact path='/naeem-website-react/' element={<About/>} />
+        <Route exact path='/naeem-website-react/projects' element={<Projects/>}/>
+        <Route exact path='/naeem-website-react/interests' element={<Interests/>}/>
       </Routes>
     </div>
   );
