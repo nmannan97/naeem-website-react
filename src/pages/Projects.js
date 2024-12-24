e4ab27dd594874c7610f4a3c78ccec95504ecbc2
Naeem Mannan <nmannan97@gmail.com>
2024-12-23 22:58:53 -0800

    making project pages changes

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -51,78 +51,86 @@ export default function Projects(){
                 
                 <ol id="projects_ProjectsList">
                     <li className="projects_ProjectItem">
-                        <p className="projects_generalFont">Radar dish ~</p>
-                        <div className="projects_projectDiv">
-                            <iframe className="projects_Video" src="https://player.vimeo.com/video/921326896?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="auto" height="auto" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="Radar dish demo"></iframe>                            
-                            <p className="projects_projectName">My radar dish</p>
-                            
-                        </div>            
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={RadarDish}/>
-                            <p className="projects_projectName">Radar dish (Side)</p>
-                        </div>   
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={RadarDish2}/>
-                            <p className="projects_projectName">Radar dish (Front)</p>
-                        </div>   
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={RadarDish3}/>
-                            <p className="projects_projectName">Assembled dish</p>
-                        </div>   
-                        <p className="projects_generalFont">
-                            2 man project where I designed a radar in solidworks, built a radar dish prototype, and automated the dish testing,
-                            while my partner did the simulations for the project in HFSS. 
-                            Along with this I designed a system that would take sample points every 1 degree and then put the average of 5 
-                            samples across various wifi channels into an excel file. This used a Python script to collect data and an Arduino 
-                            using RTOS to take accurate and communicate via serial communications. This was an SDR.
-                        </p>           
+                        <div className="projects_background">
+                            <p className="projects_generalFont">Radar dish ~</p>
+                            <div className="projects_projectDiv">
+                                <iframe className="projects_Video" src="https://player.vimeo.com/video/921326896?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="auto" height="auto" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="Radar dish demo"></iframe>                            
+                                <p className="projects_projectName">My radar dish</p>
+                                
+                            </div>            
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={RadarDish}/>
+                                <p className="projects_projectName">Radar dish (Side)</p>
+                            </div>   
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={RadarDish2}/>
+                                <p className="projects_projectName">Radar dish (Front)</p>
+                            </div>   
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={RadarDish3}/>
+                                <p className="projects_projectName">Assembled dish</p>
+                            </div>   
+                            <p className="projects_generalFont">
+                                2 man project where I designed a radar in solidworks, built a radar dish prototype, and automated the dish testing,
+                                while my partner did the simulations for the project in HFSS. 
+                                Along with this I designed a system that would take sample points every 1 degree and then put the average of 5 
+                                samples across various wifi channels into an excel file. This used a Python script to collect data and an Arduino 
+                                using RTOS to take accurate and communicate via serial communications. This was an SDR.
+                            </p> 
+                        </div>          
                     </li>
                     <li className="projects_ProjectItem">
-                        <p className="projects_generalFont">Robotic leg ~</p>
-                        <div className="projects_projectDiv">
-                            <iframe className="projects_Video" src="https://player.vimeo.com/video/921328085?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="auto" height="auto" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="RobotLeg demo"></iframe>                            
-                            <p className="projects_projectName">My robotic leg</p>
-                        </div>         
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={RobotLegPic}/>
-                            <p className="projects_projectName">Assembled Robotic leg in CREO</p>
-                        </div>   
-                        <p className="projects_generalFont">
-                            Individual robotic leg project where I designed a leg like structure in CREO parametrics and used a microcontroller to move the 
-                            leg up and down with a linear actuator and potentiometer sensing. This used CREO parametrics to design the leg and a 3D printer to build 
-                            the parts in PLA and rubber. Looking to do more with it and include servo motors to move the leg when I get 
-                            more funds.
-                        </p>              
+                        <div className="projects_background">
+                            <p className="projects_generalFont">Robotic leg ~</p>
+                            <div className="projects_projectDiv">
+                                <iframe className="projects_Video" src="https://player.vimeo.com/video/921328085?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" width="auto" height="auto" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write" title="RobotLeg demo"></iframe>                            
+                                <p className="projects_projectName">My robotic leg</p>
+                            </div>         
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={RobotLegPic}/>
+                                <p className="projects_projectName">Assembled Robotic leg in CREO</p>
+                            </div>   
+                            <p className="projects_generalFont">
+                                Individual robotic leg project where I designed a leg like structure in CREO parametrics and used a microcontroller to move the 
+                                leg up and down with a linear actuator and potentiometer sensing. This used CREO parametrics to design the leg and a 3D printer to build 
+                                the parts in PLA and rubber. Looking to do more with it and include servo motors to move the leg when I get 
+                                more funds.
+                            </p>    
+                        </div>          
                     </li>
                     <li className="projects_ProjectItem">
-                        <p className="projects_generalFont">Pip boy 3000 ~</p>
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={pipBoy0}/>
-                            <p className="projects_projectName">Pipboy 3000</p>
-                        </div>         
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={pipBoy1}/>
-                            <p className="projects_projectName">Pipboy 3000 exploded</p>
-                        </div>         
-                        <p className="projects_generalFont">
-                            Designed a pipboy 3000 using Creo Parametrics as a self started project. Made to be formed, or 3D printed, in plastics
-                            and rubbery material like NinjaFlex. 
-                        </p>              
+                        <div className="projects_background">
+                            <p className="projects_generalFont">Pip boy 3000 ~</p>
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={pipBoy0}/>
+                                <p className="projects_projectName">Pipboy 3000</p>
+                            </div>         
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={pipBoy1}/>
+                                <p className="projects_projectName">Pipboy 3000 exploded</p>
+                            </div>         
+                            <p className="projects_generalFont">
+                                Designed a pipboy 3000 using Creo Parametrics as a self started project. Made to be formed, or 3D printed, in plastics
+                                and rubbery material like NinjaFlex. 
+                            </p>    
+                        </div>          
                     </li>
                     <li className="projects_ProjectItem">
-                        <p className="projects_generalFont">Robotics club ~</p>
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={PCB0}/>
-                            <p className="projects_projectName">PCB designed for my robotics club</p>
-                        </div>         
-                        <div className="projects_projectDiv">
-                            <img className="projects_projectImage" src={PCB1}/>
-                            <p className="projects_projectName">Revision C PCB</p>
-                        </div>         
-                        <p className="projects_generalFont">
-                            This was done in DipTrace where I got to do schematic design layout as well as PCB placement of items. Worked in crossfunctional team
-                            with software, firmware, mechanical, and electrical teammates to build a Mars rover. This was done for my college project. 
-                        </p>              
+                        <div className="projects_background">
+                            <p className="projects_generalFont">Robotics club ~</p>
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={PCB0}/>
+                                <p className="projects_projectName">PCB designed for my robotics club</p>
+                            </div>         
+                            <div className="projects_projectDiv">
+                                <img className="projects_projectImage" src={PCB1}/>
+                                <p className="projects_projectName">Revision C PCB</p>
+                            </div>         
+                            <p className="projects_generalFont">
+                                This was done in DipTrace where I got to do schematic design layout as well as PCB placement of items. Worked in crossfunctional team
+                                with software, firmware, mechanical, and electrical teammates to build a Mars rover. This was done for my college project. 
+                            </p>   
+                        </div>           
                     </li>
                 </ol>
 
