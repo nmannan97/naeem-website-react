2d4a236c64e58809284d1bd7fe654bb8e3675e07
Naeem Mannan <nmannan97@gmail.com>
2025-05-20 17:11:56 -0700

    adding pong backend stuff to the website

--- src/pages/Pong/Animations/AnimationsBall.js
+++ src/pages/Pong/Animations/AnimationsBall.js
@@ -139,7 +139,7 @@ export default function AnimationsBall() {
   useEffect(() => {
     const postScore = async () => {
       try {
-        const response = await axios.post('ec2-35-89-227-222.us-west-2.compute.amazonaws.com/submit-score', {
+        const response = await axios.post('http://35.89.227.222:5000/submit-score', {
           score: score
         });
         console.log('Score posted:', response.data);
